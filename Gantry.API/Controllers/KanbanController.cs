716bebf7a5e346424a1dd3a76101c6a6ec708125
Kamil Wojtol <kamil.wojtol97@gmail.com>
2026-09-09 21:23:49 +0200

    added endpoint for kanban creation

--- Gantry.API/Controllers/KanbanController.cs
+++ Gantry.API/Controllers/KanbanController.cs
@@ -1,5 +1,6 @@
 ﻿using Gantry.API.Data;
 using Gantry.API.Dtos;
+using Gantry.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Gantry.API.Controllers
@@ -39,6 +40,21 @@ namespace Gantry.API.Controllers
             }
 
             _dbContext.KanbanBoards.Remove(foundKanban);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult CreateKanban(EditKanbanDto kanbanBoard)
+        {
+            var newKanban = new Kanban()
+            {
+                Id = _dbContext.KanbanBoards.Count() + 1,
+                Title = kanbanBoard.Title,
+                Tasks = new List<Models.Task>()
+            }; 
+
+            _dbContext.KanbanBoards.Add(newKanban);
 
             return Ok();
         }
